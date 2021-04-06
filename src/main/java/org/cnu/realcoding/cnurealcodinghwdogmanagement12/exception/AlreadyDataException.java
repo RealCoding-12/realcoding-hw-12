19262c4262f76e7adb735a067abb0fa3f6ba1261
DESKTOP-E5Q7HVL\dlenr <dlenrl67@gmail.com>
2021-04-07 05:21:46 +0900

    alreadyData, ChangeMedicalRecords Exception

--- src/main/java/org/cnu/realcoding/cnurealcodinghwdogmanagement12/exception/AlreadyDataException.java
+++ src/main/java/org/cnu/realcoding/cnurealcodinghwdogmanagement12/exception/AlreadyDataException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
-public class AreadyDataException extends RuntimeException {
+public class AlreadyDataException extends RuntimeException {
 }
 
