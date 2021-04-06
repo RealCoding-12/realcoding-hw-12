94627a27847203a2a09f960f5c7bb63dfafd421b
kdhur <1kdhur2@gmail.com>
2021-04-07 03:10:46 +0900

    DogManagementService에서 이용할 함수 구현

---
+++ src/main/java/org/cnu/realcoding/cnurealcodinghwdogmanagement12/repository/DogRepository.java
@@ -0,0 +1,28 @@
+package org.cnu.realcoding.cnurealcodinghwdogmanagement12.repository;
+
+import org.cnu.realcoding.cnurealcodinghwdogmanagement12.domain.Dog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class DogRepository {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Dog findDog(String name) {
+        return mongoTemplate.findOne(Query.query(Criteria.where("name").is(name)), Dog.class);
+    }
+
+    public void insertDog(Dog dog) {
+        mongoTemplate.insert(dog);
+    }
+
+    public List<Dog> findAllDogs() {
+        return mongoTemplate.findAll(Dog.class);
+    }
+}
