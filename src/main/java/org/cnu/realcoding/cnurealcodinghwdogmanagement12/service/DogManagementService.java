c804c704e6a45e8a562e7b4eb3f6ae4bd005e97c
DESKTOP-E5Q7HVL\dlenr <dlenrl67@gmail.com>
2021-04-07 03:34:04 +0900

    exception package에 함수구현

--- src/main/java/org/cnu/realcoding/cnurealcodinghwdogmanagement12/service/DogManagementService.java
+++ src/main/java/org/cnu/realcoding/cnurealcodinghwdogmanagement12/service/DogManagementService.java
@@ -1,6 +1,7 @@
 package org.cnu.realcoding.cnurealcodinghwdogmanagement12.service;
 
 import org.cnu.realcoding.cnurealcodinghwdogmanagement12.domain.Dog;
+import org.cnu.realcoding.cnurealcodinghwdogmanagement12.exception.DogNotFoundException;
 import org.cnu.realcoding.cnurealcodinghwdogmanagement12.repository.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,9 +19,9 @@ public class DogManagementService {
     public Dog getDogByName(String name) {
         Dog dog = dogRepository.findDog(name);
 
-        /*if(dog == null) {
+        if(dog == null) {
             throw new DogNotFoundException();
-        }*/
+        }
 
         return dog;
     }
